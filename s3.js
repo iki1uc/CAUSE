23649540f338727dbe357b87d74901d60e32fcd5
iki1uc <entertrainer@web.de>
2026-08-17 04:14:13 +0200

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,6 +1,4 @@
 function WHIRL_START(pool) {
-    // alle Module dürfen normal sein
-    // aber wenn WHIRL startet → CAUSE = 3
-    pool.causeLevel = 3;
+    pool.causeLevel = 3; // WHIRL startet IMMER bei 3
     return "WHIRL läuft jetzt im CAUSE‑Modus (3)";
 }
